c7675e263c8013f4a858c168b1f1b9ab471e977f
AlexCorpROS <arcorprintr@gmail.com>
2022-11-30 20:16:26 +0300

    add task2 ex9,ex11

--- homework/task2/example10.py
+++ homework/task2/example10.py
@@ -1,14 +1,24 @@
 """ Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов
  на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число. """
 
-from random import randint
+import random
 user_num = int(input('Введите количесвто элементов списка: '))
 user_list = []
 for i in range(user_num):
-    user_list.append(randint(-user_num,user_num))
+    user_list.append(random.randint(-user_num,user_num))
 print(f'Полученный список имеет вид: {user_list}')
 
-x = open('file.txt','r')
-print(x)
-""" result = user_list[int(x.readline())] * numbers[int(x.readline(2))]
-print(result) """
+# задаем значения строк в файле file.txt и считываем их в список indexs
+with open('file.txt', 'w') as data:
+    for i in range(user_num):
+        data.write(f"{random.randrange(0, user_num)}\n")
+
+with open('file.txt', 'r') as data:
+    indexs = list(map(int,data.readlines()))
+print(f'Полученные индексы {indexs}')
+
+# Перемножаем между собой элементы user_list определенные в последовательности indexs
+composition = 1
+for i in range(user_num):
+    composition *= user_list[indexs[i]]
+print(f'Произведение элементов на указанных позициях равно {composition}')
--- homework/task2/example11.py
+++ homework/task2/example11.py
@@ -1,2 +1,12 @@
 """ Реализуйте алгоритм перемешивания списка. """
 
+import random
+user_num = int(input('Введите количесвто элементов списка: '))
+user_list = []
+for i in range(user_num):
+    user_list.append(random.randint(-user_num,user_num))
+print(f'Полученный список имеет вид: {user_list}')
+random.shuffle(user_list)
+print(f"Cписок после первого перемешивания:{user_list}")
+random.shuffle(user_list)
+print(f"Cписок после второго перемешивания:{user_list}")
